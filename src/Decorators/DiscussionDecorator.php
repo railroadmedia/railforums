61380fd187d88ec85c94897d3cddb75df11e79b9
Roxana Riza <roxana_r2000@yahoo.com>
2021-04-05 19:02:06 +0300

    Add latest posts

--- src/Decorators/DiscussionDecorator.php
+++ src/Decorators/DiscussionDecorator.php
@@ -27,36 +27,72 @@ class DiscussionDecorator implements DecoratorInterface
     public function decorate($discussions)
     {
         foreach ($discussions as $discussion) {
-            $lastPost =
+            $latestPosts = $this->databaseManager->connection(config('railforums.database_connection'))
+                ->table(ConfigService::$tablePosts)
+                ->select(ConfigService::$tablePosts.'.id','thread_id', 'author_id', DB::raw('MAX(updated_at) as last_post_created_at'))
+                ->whereNull('deleted_at')
+                ->groupBy('thread_id');
+
+            $threads =
                 $this->databaseManager->connection(config('railforums.database_connection'))
                     ->table(ConfigService::$tableCategories)
+                    ->select(
+                        [
+                            ConfigService::$tableCategories.'.id as category_id',
+                            'threads.*',
+                            'latest_posts.id as post_id',
+                            'latest_posts.last_post_created_at as last_post_created_at',
+                            'latest_posts.author_id as latest_post_author_id',
+                        ]
+                    )
                     ->leftJoin(
-                        ConfigService::$tableThreads,
+                        ConfigService::$tableThreads . ' as threads',
                         ConfigService::$tableCategories . '.id',
                         '=',
-                        ConfigService::$tableThreads . '.category_id'
+                         'threads.category_id'
                     )
-                    ->leftJoin(
-                        ConfigService::$tablePosts,
-                        function ($leftJoin) {
-                            $leftJoin->on(
-                                ConfigService::$tableThreads . '.id',
-                                '=',
-                                ConfigService::$tablePosts . '.thread_id'
-                            )
-                                ->where(
-                                    ConfigService::$tablePosts . '.created_at',
-                                    '=',
-                                    DB::raw(
-                                        "(select max(`created_at`) from " . ConfigService::$tablePosts . ")"
-                                    )
-                                );
-                        }
-                    )
+                    ->leftJoinSub($latestPosts, 'latest_posts', function ($join) {
+                        $join->on( 'threads.id', '=', 'latest_posts.thread_id');
+                    })
                     ->where(ConfigService::$tableCategories . '.id', $discussion['id'])
                     ->get();
 
+            $userIds = array_unique($threads->pluck('author_id')
+                ->toArray());
+
+            $users =
+                $this->databaseManager->connection(config('railforums.author_database_connection'))
+                    ->table(config('railforums.author_table_name'))
+                    ->select(
+                        [
+                            config('railforums.author_table_id_column_name'),
+                            config('railforums.author_table_display_name_column_name'),
+                            config('railforums.author_table_avatar_column_name'),
+                        ]
+                    )
+                    ->whereIn(config('railforums.author_table_id_column_name'), $userIds)
+                    ->get()
+                    ->keyBy(config('railforums.author_table_id_column_name'));
+
+            $displayNameColumnName = config('railforums.author_table_display_name_column_name');
+            $avatarUrlColumnName = config('railforums.author_table_avatar_column_name');
+
+             foreach ($threads as $thread){
+                if($thread->post_id){
+                    $discussion['latest_post']['id'] = $thread->post_id;
+                    $discussion['latest_post']['created_at'] = $thread->last_post_created_at;
+                    $discussion['latest_post']['thread_title'] = $thread->title;
+                    $discussion['latest_post']['author_id'] = $thread->latest_post_author_id;
+                    if (!empty($users[$thread->latest_post_author_id])) {
+                        $user = $users[$thread->latest_post_author_id];
+                        $discussion['latest_post']['author_display_name'] = $user->$displayNameColumnName;
+                        $discussion['latest_post']['author_avatar_url'] =
+                            $user->$avatarUrlColumnName ?? config('railforums.author_default_avatar_url');
+                    }
+                }
+            }
         }
+
         return $discussions;
     }
 }
