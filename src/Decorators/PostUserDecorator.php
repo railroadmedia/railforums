cbcdbab2d6783e64d5a53c892a4ed929170d9500
Roxana Riza <roxana_r2000@yahoo.com>
2021-04-27 19:19:59 +0300

    Add author signature on posts

--- src/Decorators/PostUserDecorator.php
+++ src/Decorators/PostUserDecorator.php
@@ -21,8 +21,16 @@ class PostUserDecorator implements DecoratorInterface
      */
     private $userProvider;
 
-    public function __construct(DatabaseManager $databaseManager, UserProviderInterface $userProvider)
-    {
+    /**
+     * PostUserDecorator constructor.
+     *
+     * @param DatabaseManager $databaseManager
+     * @param UserProviderInterface $userProvider
+     */
+    public function __construct(
+        DatabaseManager $databaseManager,
+        UserProviderInterface $userProvider
+    ) {
         $this->databaseManager = $databaseManager;
         $this->userProvider = $userProvider;
     }
@@ -52,9 +60,21 @@ class PostUserDecorator implements DecoratorInterface
 
         $users = $this->userProvider->getUsersByIds($userIds);
 
+        $signatures =   $this->databaseManager->connection(config('railforums.database_connection'))
+            ->table(ConfigService::$tableUserSignatures)
+            ->select('user_id', 'signature')
+            ->whereIn(ConfigService::$tableUserSignatures . '.user_id', $userIds)
+            ->where('brand', config('railforums.brand'))
+            ->groupBy('user_id')
+            ->get()
+            ->toArray();
+
+        $userSignatures = array_combine(array_column($signatures, 'user_id'), array_column($signatures, 'signature'));
+
         foreach ($posts as $postIndex => $post) {
 
             if (!empty($users[$post['author_id']])) {
+
                 $user = $users[$post['author_id']];
 
                 $posts[$postIndex]['author_display_name'] = $user->getDisplayName();
@@ -64,6 +84,7 @@ class PostUserDecorator implements DecoratorInterface
                 $posts[$postIndex]['author_days_as_member'] =
                     Carbon::parse($user->getCreatedAt())
                         ->diffInDays(Carbon::now());
+                $posts[$postIndex]['author_signature'] = $userSignatures[$post['author_id']] ?? null;
             }
         }
 
