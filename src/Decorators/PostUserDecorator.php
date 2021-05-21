5d1becf8a78cea0584fa57359e3007d771a216a2
Roxana Riza <roxana_r2000@yahoo.com>
2021-05-21 13:53:47 +0300

    Add posts likes and level rank to member stats

--- src/Decorators/PostUserDecorator.php
+++ src/Decorators/PostUserDecorator.php
@@ -70,32 +70,43 @@ class PostUserDecorator implements DecoratorInterface
                 ->select('user_id', 'signature')
                 ->whereIn(ConfigService::$tableUserSignatures . '.user_id', $userIds)
                 ->where('brand', config('railforums.brand'))
-                //->groupBy('user_id')
                 ->get()
                 ->toArray();
 
         $userSignatures = array_combine(array_column($signatures, 'user_id'), array_column($signatures, 'signature'));
 
+        $postLikes =
+            $this->databaseManager->connection(config('railforums.database_connection'))
+                ->table(ConfigService::$tablePostLikes)
+                ->selectRaw('COUNT(' . ConfigService::$tablePostLikes . '.id) as count')
+                ->addSelect('liker_id')
+                ->whereIn(ConfigService::$tablePostLikes . '.liker_id', $userIds)
+                ->get()
+                ->toArray();
+
+        $userLikes = array_combine(array_column($postLikes, 'liker_id'), array_column($postLikes, 'count'));
+
         foreach ($posts as $postIndex => $post) {
 
             if (!empty($users[$post['author_id']])) {
-
                 $user = $users[$post['author_id']];
-
-                $posts[$postIndex]['author_display_name'] = $user->getDisplayName();
-                $posts[$postIndex]['author_avatar_url'] =
+                $posts[$postIndex]['author']['display_name'] = $user->getDisplayName();
+                $posts[$postIndex]['author']['avatar_url'] =
                     $user->getProfilePictureUrl() ?? config('railforums.author_default_avatar_url');
-                $posts[$postIndex]['author_total_posts'] = $userPosts[$post['author_id']] ?? 0;
-                $posts[$postIndex]['author_days_as_member'] =
+                $posts[$postIndex]['author']['total_posts'] = $userPosts[$post['author_id']] ?? 0;
+                $posts[$postIndex]['author']['days_as_member'] =
                     Carbon::parse($user->getCreatedAt())
                         ->diffInDays(Carbon::now());
-                $posts[$postIndex]['author_signature'] = $userSignatures[$post['author_id']] ?? null;
-                $posts[$postIndex]['author_access_level'] = $usersAccessLevel[$post['author_id']] ?? null;
-                $posts[$postIndex]['author_xp'] =
+                $posts[$postIndex]['author']['signature'] = $userSignatures[$post['author_id']] ?? null;
+                $posts[$postIndex]['author']['access_level'] = $usersAccessLevel[$post['author_id']] ?? null;
+                $posts[$postIndex]['author']['xp'] =
                     (array_key_exists($post['author_id'], $usersXp)) ? $usersXp[$post['author_id']]['xp'] : 0;
-                $posts[$postIndex]['author_xp_rank'] =
-                    (array_key_exists($post['author_id'], $usersXp)) ? $usersXp[$post['author_id']]['xp_rank'] :
-                        'Pianote Member';
+                $posts[$postIndex]['author']['xp_rank'] = $usersXp[$post['author_id']]['xp_rank'];
+                $posts[$postIndex]['author']['total_post_likes'] = $userLikes[$post['author_id']] ?? 0;
+                $posts[$postIndex]['author']['created_at'] =
+                    $user->getCreatedAt()
+                        ->toDateTimeString();
+                $posts[$postIndex]['author']['level_rank'] = $usersXp['level_rank'];
             }
         }
 
