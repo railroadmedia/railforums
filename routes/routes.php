077831c38faa839e25ce2e9a1f608d7802dd7b22
calebfavor <calebfavor@gmail.com>
2021-06-11 11:59:15 -0700

    Missing commit data from last commit

--- routes/routes.php
+++ routes/routes.php
@@ -1,235 +1,244 @@
 <?php
-//discussions
-Route::put(
-    'discussion/store',
-    \Railroad\Railforums\Controllers\UserForumDiscussionController::class . '@store'
-)->name('railforums.discussion.store');
-Route::patch(
-    'discussion/update/{id}',
-    \Railroad\Railforums\Controllers\UserForumDiscussionController::class . '@update'
-)->name('railforums.discussion.update');
-Route::delete(
-    'discussion/delete/{id}',
-    \Railroad\Railforums\Controllers\UserForumDiscussionController::class . '@delete'
-)->name('railforums.discussion.delete');
-
-// threads
-Route::put(
-    'thread/store',
-    \Railroad\Railforums\Controllers\UserForumThreadController::class . '@store'
-)->name('railforums.thread.store');
-
-Route::patch(
-    'thread/update/{id}',
-    \Railroad\Railforums\Controllers\UserForumThreadController::class . '@update'
-)->name('railforums.thread.update');
-
-// threads follow
-Route::put(
-    'thread/follow/{id}',
-    \Railroad\Railforums\Controllers\UserForumThreadController::class . '@follow'
-)->name('railforums.thread.follow');
-
-Route::delete(
-    'thread/unfollow/{id}',
-    \Railroad\Railforums\Controllers\UserForumThreadController::class . '@unfollow'
-)->name('railforums.thread.unfollow');
-
-// threads read
-Route::put(
-    'thread/read/{id}',
-    \Railroad\Railforums\Controllers\UserForumThreadController::class . '@read'
-)->name('railforums.thread.read');
-
-// threads delete
-Route::delete(
-    'thread/delete/{id}',
-    \Railroad\Railforums\Controllers\UserForumThreadController::class . '@delete'
-)->name('railforums.thread.delete');
-
-
-// posts
-Route::put(
-    'post/store',
-    \Railroad\Railforums\Controllers\UserForumPostController::class . '@store'
-)->name('railforums.post.store');
-
-Route::patch(
-    'post/update/{id}',
-    \Railroad\Railforums\Controllers\UserForumPostController::class . '@update'
-)->name('railforums.post.update');
-
-// post likes
-Route::put(
-    'post/like/{id}',
-    \Railroad\Railforums\Controllers\UserForumPostController::class . '@like'
-)->name('railforums.post.like');
-
-Route::delete(
-    'post/unlike/{id}',
-    \Railroad\Railforums\Controllers\UserForumPostController::class . '@unlike'
-)->name('railforums.post.unlike');
 
 Route::group(
     [
-        'middleware' => config('railforums.route_middleware_logged_in_groups',[]),
+        'prefix' => 'forums/',
+        'middleware' => config('railforums.controller_middleware', []),
     ],
     function () {
-        //user signatures
+        // discussions
         Route::put(
-            'signature/store',
-            \Railroad\Railforums\Controllers\UserForumSignaturesController::class . '@store'
-        )
-            ->name('railforums.signature.store');
+            'discussion/store',
+            \Railroad\Railforums\Controllers\UserForumDiscussionController::class.'@store'
+        )->name('railforums.discussion.store');
+        Route::patch(
+            'discussion/update/{id}',
+            \Railroad\Railforums\Controllers\UserForumDiscussionController::class.'@update'
+        )->name('railforums.discussion.update');
+        Route::delete(
+            'discussion/delete/{id}',
+            \Railroad\Railforums\Controllers\UserForumDiscussionController::class.'@delete'
+        )->name('railforums.discussion.delete');
 
-Route::patch(
-    'signature/update/{id}',
-    \Railroad\Railforums\Controllers\UserForumSignaturesController::class . '@update'
-)
-    ->name('railforums.signature.update');
+        // threads
+        Route::put(
+            'thread/store',
+            \Railroad\Railforums\Controllers\UserForumThreadController::class.'@store'
+        )->name('railforums.thread.store');
 
-Route::delete(
-    'signature/delete/{id}',
-    \Railroad\Railforums\Controllers\UserForumSignaturesController::class . '@delete'
-)
-    ->name('railforums.discussions.delete');
+        Route::patch(
+            'thread/update/{id}',
+            \Railroad\Railforums\Controllers\UserForumThreadController::class.'@update'
+        )->name('railforums.thread.update');
+
+        // threads follow
+        Route::put(
+            'thread/follow/{id}',
+            \Railroad\Railforums\Controllers\UserForumThreadController::class.'@follow'
+        )->name('railforums.thread.follow');
+
+        Route::delete(
+            'thread/unfollow/{id}',
+            \Railroad\Railforums\Controllers\UserForumThreadController::class.'@unfollow'
+        )->name('railforums.thread.unfollow');
+
+        // threads read
+        Route::put(
+            'thread/read/{id}',
+            \Railroad\Railforums\Controllers\UserForumThreadController::class.'@read'
+        )->name('railforums.thread.read');
+
+        // threads delete
+        Route::delete(
+            'thread/delete/{id}',
+            \Railroad\Railforums\Controllers\UserForumThreadController::class.'@delete'
+        )->name('railforums.thread.delete');
+
+
+        // posts
+        Route::put(
+            'post/store',
+            \Railroad\Railforums\Controllers\UserForumPostController::class.'@store'
+        )->name('railforums.post.store');
+
+        Route::patch(
+            'post/update/{id}',
+            \Railroad\Railforums\Controllers\UserForumPostController::class.'@update'
+        )->name('railforums.post.update');
+
+        // post likes
+        Route::put(
+            'post/like/{id}',
+            \Railroad\Railforums\Controllers\UserForumPostController::class.'@like'
+        )->name('railforums.post.like');
+
+        Route::delete(
+            'post/unlike/{id}',
+            \Railroad\Railforums\Controllers\UserForumPostController::class.'@unlike'
+        )->name('railforums.post.unlike');
+
+        Route::group(
+            [
+                'middleware' => config('railforums.route_middleware_logged_in_groups', []),
+            ],
+            function () {
+                //user signatures
+                Route::put(
+                    'signature/store',
+                    \Railroad\Railforums\Controllers\UserForumSignaturesController::class.'@store'
+                )
+                    ->name('railforums.signature.store');
+
+                Route::patch(
+                    'signature/update/{id}',
+                    \Railroad\Railforums\Controllers\UserForumSignaturesController::class.'@update'
+                )
+                    ->name('railforums.signature.update');
+
+                Route::delete(
+                    'signature/delete/{id}',
+                    \Railroad\Railforums\Controllers\UserForumSignaturesController::class.'@delete'
+                )
+                    ->name('railforums.discussions.delete');
+            }
+        );
     }
 );
 
 Route::group(
     [
         'prefix' => 'forums/',
-        'middleware' => config('railforums.controller_middleware',[]),
+        'middleware' => config('railforums.controller_middleware', []),
     ],
     function () {
         Route::get(
             'thread/index',
-            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class . '@index'
+            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class.'@index'
         )->name('railforums.thread.index');
 
         Route::get(
             'thread/show/{id}',
-            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class . '@show'
+            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class.'@show'
         )->name('railforums.mobile.thread.show');
 
         Route::put(
             'thread/store',
-            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class . '@store'
+            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class.'@store'
         )->name('railforums.api.thread.store');
 
         Route::patch(
             'thread/update/{id}',
-            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class . '@update'
+            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class.'@update'
         )->name('railforums.api.thread.update');
 
         Route::delete(
             'thread/delete/{id}',
-            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class . '@delete'
+            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class.'@delete'
         )->name('railforums.api.thread.delete');
 
         Route::put(
             'thread/follow/{id}',
-            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class . '@follow'
+            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class.'@follow'
         )->name('railforums.api.thread.follow');
 
         Route::delete(
             'thread/unfollow/{id}',
-            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class . '@unfollow'
+            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class.'@unfollow'
         )->name('railforums.api.thread.unfollow');
 
         Route::put(
             'thread/read/{id}',
-            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class . '@read'
+            \Railroad\Railforums\Controllers\UserForumThreadJsonController::class.'@read'
         )->name('railforums.thread.read');
 
         // post api
         Route::get(
             'post/index',
-            \Railroad\Railforums\Controllers\UserForumPostJsonController::class . '@index'
+            \Railroad\Railforums\Controllers\UserForumPostJsonController::class.'@index'
         )->name('railforums.api.post.index');
 
         Route::get(
             'post/show/{id}',
-            \Railroad\Railforums\Controllers\UserForumPostJsonController::class . '@show'
+            \Railroad\Railforums\Controllers\UserForumPostJsonController::class.'@show'
         )->name('railforums.api.post.show');
 
         Route::put(
             'post/store',
-            \Railroad\Railforums\Controllers\UserForumPostJsonController::class . '@store'
+            \Railroad\Railforums\Controllers\UserForumPostJsonController::class.'@store'
         )->name('railforums.api.post.store');
 
         Route::patch(
             'post/update/{id}',
-            \Railroad\Railforums\Controllers\UserForumPostJsonController::class . '@update'
+            \Railroad\Railforums\Controllers\UserForumPostJsonController::class.'@update'
         )->name('railforums.api.post.update');
 
         Route::delete(
             'post/delete/{id}',
-            \Railroad\Railforums\Controllers\UserForumPostJsonController::class . '@delete'
+            \Railroad\Railforums\Controllers\UserForumPostJsonController::class.'@delete'
         )->name('railforums.api.post.delete');
 
         Route::put(
             'post/report/{id}',
-            \Railroad\Railforums\Controllers\UserForumPostJsonController::class . '@report'
+            \Railroad\Railforums\Controllers\UserForumPostJsonController::class.'@report'
         )->name('railforums.api.post.report');
 
         // post likes
         Route::put(
             'post/like/{id}',
-            \Railroad\Railforums\Controllers\UserForumPostJsonController::class . '@like'
+            \Railroad\Railforums\Controllers\UserForumPostJsonController::class.'@like'
         )->name('railforums.api.post.like');
 
         Route::delete(
             'post/unlike/{id}',
-            \Railroad\Railforums\Controllers\UserForumPostJsonController::class . '@unlike'
+            \Railroad\Railforums\Controllers\UserForumPostJsonController::class.'@unlike'
         )->name('railforums.api.post.unlike');
 
         // search
         Route::get(
             'search',
-            \Railroad\Railforums\Controllers\UserForumSearchJsonController::class . '@index'
+            \Railroad\Railforums\Controllers\UserForumSearchJsonController::class.'@index'
         )->name('railforums.api.search.index');
 
         //categories
         Route::put(
             'discussions/store',
-            \Railroad\Railforums\Controllers\UserForumDiscussionJsonController::class . '@store'
+            \Railroad\Railforums\Controllers\UserForumDiscussionJsonController::class.'@store'
         )->name('railforums.api.discussion.store');
 
         Route::get(
             'discussions/show/{id}',
-            \Railroad\Railforums\Controllers\UserForumDiscussionJsonController::class . '@show'
+            \Railroad\Railforums\Controllers\UserForumDiscussionJsonController::class.'@show'
         )->name('railforums.api.discussion.show');
 
         Route::get(
             'discussions/index',
-            \Railroad\Railforums\Controllers\UserForumDiscussionJsonController::class . '@index'
+            \Railroad\Railforums\Controllers\UserForumDiscussionJsonController::class.'@index'
         )->name('railforums.api.discussions.index');
 
         Route::patch(
             'discussions/update/{id}',
-            \Railroad\Railforums\Controllers\UserForumDiscussionJsonController::class . '@update'
+            \Railroad\Railforums\Controllers\UserForumDiscussionJsonController::class.'@update'
         )->name('railforums.api.discussions.update');
 
         Route::delete(
             'discussions/delete/{id}',
-            \Railroad\Railforums\Controllers\UserForumDiscussionJsonController::class . '@delete'
+            \Railroad\Railforums\Controllers\UserForumDiscussionJsonController::class.'@delete'
         )->name('railforums.api.discussions.delete');
 
         //user signatures
         Route::put(
             'signature/store',
-            \Railroad\Railforums\Controllers\UserForumSignaturesJsonController::class . '@store'
+            \Railroad\Railforums\Controllers\UserForumSignaturesJsonController::class.'@store'
         )->name('railforums.api.signature.store');
 
         Route::patch(
             'signature/update/{id}',
-            \Railroad\Railforums\Controllers\UserForumSignaturesJsonController::class . '@update'
+            \Railroad\Railforums\Controllers\UserForumSignaturesJsonController::class.'@update'
         )->name('railforums.api.signature.update');
 
         Route::delete(
             'signature/delete/{id}',
-            \Railroad\Railforums\Controllers\UserForumSignaturesJsonController::class . '@delete'
+            \Railroad\Railforums\Controllers\UserForumSignaturesJsonController::class.'@delete'
         )->name('railforums.api.discussions.delete');
-
-    });
+    }
+);
