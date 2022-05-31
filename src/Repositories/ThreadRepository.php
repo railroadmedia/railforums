b9c1b9f8c9a28cc55a0c17f85585b4d01c26e550
calebfavor <calebfavor@gmail.com>
2022-05-31 09:24:33 -0700

    Fixing threads without a last post id still being pulled. They shouldn't be.

--- src/Repositories/ThreadRepository.php
+++ src/Repositories/ThreadRepository.php
@@ -332,6 +332,7 @@ class ThreadRepository extends EventDispatchingRepository
                 },
                 'is_followed'
             )
+            ->havingNotNull(['last_post_id'])
             ->whereNull(ConfigService::$tableThreads . '.deleted_at')
             ->whereNull(ConfigService::$tableCategories . '.deleted_at');
     }
