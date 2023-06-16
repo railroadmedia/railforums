cf7ad9770eef5dad3021e85a988bffec74bc3a4d
Alexandre Severo <alexandre@musora.com>
2023-06-16 15:12:33 -0700

    Formatting

--- src/Middleware/SignatureHTMLSyntaxCheck.php
+++ src/Middleware/SignatureHTMLSyntaxCheck.php
@@ -8,13 +8,16 @@ use Closure;
 
 class SignatureHTMLSyntaxCheck
 {
-    public function handle(Request $request, Closure $next) {
+    public function handle(Request $request, Closure $next)
+    {
         libxml_use_internal_errors(true);
         $doc = new DOMDocument();
         $ret = $doc->loadXML('<div>' . html_entity_decode($request->get('signature')) . '</div>');
         libxml_clear_errors();
         if (!$ret) {
-           return redirect()->back()->with('error', 'Invalid signature. Please check its syntax.');
+            return redirect()
+                ->back()
+                ->with('error', 'Invalid signature. Please check its syntax.');
         }
         return $next($request);
     }
