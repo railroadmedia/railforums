636e22fffa1cacb988d4a19335662cfe87ba5cd2
Alexandre Severo <alexandre@musora.com>
2023-06-21 16:55:32 -0700

    Disable caching on HTMLPurifing

--- config/railforums.php
+++ config/railforums.php
@@ -85,7 +85,10 @@ return [
                 'CSS.AllowedProperties' => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
                 'AutoFormat.AutoParagraph' => true,
                 'AutoFormat.RemoveEmpty' => true,
-                'Cache.DefinitionImpl' => null
+                'Cache.DefinitionImpl' => null,
+                'Core.DefinitionCache' => null,
+                'Cache.SerializerPermissions' => null,
+                'Cache.SerializerPath' => null
             ],
         ],
     ],
